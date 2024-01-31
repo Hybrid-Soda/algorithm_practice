2af04c161c341e0291ab025aa9fa88b4816e24b8
limkt <limkt0925@naver.com>
2024-01-31 11:48:37 +0900

    Self-Study

--- swea/algorithm_lecture/00_List_I/4828_min_max/4828.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdin = open("input.txt")
-
-for tc in range(1, int(input())+1):
-    N = int(input())
-    arr = list(map(int, input().split()))
-    print(f'#{tc} {max(arr) - min(arr)}')
