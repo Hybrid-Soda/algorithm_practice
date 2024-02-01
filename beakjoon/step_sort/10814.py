fa5b073fd00074c7d4852600bee56e353131c02b
lim kyoung tae <limkt0925@gmail.com>
2024-02-01 23:16:54 +0900

    UPDATE Algoritm practice

--- beakjoon/step_sort/10814.py
+++ beakjoon/step_sort/10814.py
@@ -5,14 +5,13 @@ sys.stdin = open('input.txt')
 # 1. 나이 오른차순 / 2. 이름 먼저 가입한 사람 순
 # 이름 먼저 가입한 사람 순 keep하는 방법 -> 그냥 버블정렬 해야할듯
 
-N = int(input())
-arr = [input().split() for _ in range(N)]
+N = int(sys.stdin.readline())
+arr = []
+for _ in range(N):
+	age, name = sys.stdin.readline().split()
+	arr.append([int(age), name])
 
-def asc(arr, N):
-	for i in range(N-1, 0, -1):
-		for j in range(i):
-			if int(arr[j][0]) > int(arr[j+1][0]):
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-	return arr
+arr.sort(key= lambda x: x[0])
 
-print(asc(arr, N))
+for i in arr:
+	print(i[0],i[1])
