12f960a481c34af975faf8c0a35e67ca46ae05c4
limkt <limkt0925@gmail.com>
2024-02-21 17:59:19 +0900

    240221 UPDATE

--- algorithm_study/week4/baekjoon_1891.py
+++ algorithm_study/week4/baekjoon_1891.py
@@ -3,8 +3,41 @@
 import sys
 sys.stdin = open("input.txt")
 
-d, num = map(int, input().split())
+# 주어진 번호로 인덱스 추적
+def get_coor(n, idx, row, col):
+    # 탈출 조건: 분할 완료 시
+    if n == 0: return row, col
+    # 반복 분할
+    if num[idx] == '1': return get_coor(n//2, idx+1, row, col + n)
+    elif num[idx] == '2': return get_coor(n//2, idx+1, row, col)
+    elif num[idx] == '3': return get_coor(n//2, idx+1, row + n, col)
+    elif num[idx] == '4': return get_coor(n//2, idx+1, row + n, col + n)
+
+# 사분면 번호 제작
+def make_num(n, i, j, num_v):
+    print('hi')
+    # 탈출 조건 : 자릿수가 채워질 경우
+    if len(num_v) == d: print(num_v); return num_v
+    # 반복 분할
+    if (i <= row <= i + n//2) and (j + n//2 < col < j + n):
+        return make_num(n//2, i, j + n//2, num_v + '1')
+    elif (i <= row <= i + n//2) and (j <= col <= j + n//2):
+        return make_num(n//2, i, j, num_v + '2')
+    elif (i + n//2 < row < i + n) and (j <= col <= j + n//2):
+        return make_num(n//2, i + n//2, j, num_v + '3')
+    elif (i + n//2 < row < i + n) and (j + n//2 < col < j + n):
+        return make_num(n//2, i + n//2, j + n//2, num_v + '4')
+
+
+d, num = input().split(); d = int(d)
 x, y = map(int, input().split())
+
+row, col = get_coor(2**d//2, 0, 0, 0)
+row += x; col += y
+print(make_num(2**d, 0, 0, ''))
+
+
+
 
 # 메모리 초과
 '''
@@ -12,7 +45,6 @@ def div_conq(n, i, j, quad):
     if len(quad) == d:
         arr[i][j] = quad
         return
-
     div_conq(n//2, i, j + n, quad + "1")
     div_conq(n//2, i, j, quad + "2")
     div_conq(n//2, i + n, j, quad + "3")
