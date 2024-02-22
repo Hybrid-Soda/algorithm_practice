081693e9a2a2bb2acbaec1632080e0310a76699e
lim kyoung tae <limkt0925@gmail.com>
2024-02-23 00:37:39 +0900

    240223 UPDATE

--- algorithm_study/week4/baekjoon_1891.py
+++ algorithm_study/week4/baekjoon_1891.py
@@ -1,42 +1,33 @@
 # 1891 / 사분면 / 골드4
 
-import sys
-sys.stdin = open("input.txt")
-
-# 주어진 번호로 인덱스 추적
-def get_coor(n, idx, row, col):
-    # 탈출 조건: 분할 완료 시
-    if n == 0: return row, col
-    # 반복 분할
-    if num[idx] == '1': return get_coor(n//2, idx+1, row, col + n)
-    elif num[idx] == '2': return get_coor(n//2, idx+1, row, col)
-    elif num[idx] == '3': return get_coor(n//2, idx+1, row + n, col)
-    elif num[idx] == '4': return get_coor(n//2, idx+1, row + n, col + n)
+# 인덱스 추적
+def get_idx(n, i=0, r=0, c=0):
+    if n < 1: return r-y, c+x
+    if num[i] == '1': return get_idx(n//2, i+1, r, c+n)
+    elif num[i] == '2': return get_idx(n//2, i+1, r, c)
+    elif num[i] == '3': return get_idx(n//2, i+1, r+n, c)
+    elif num[i] == '4': return get_idx(n//2, i+1, r+n, c+n)
 
 # 사분면 번호 제작
-def make_num(n, i, j, num_v):
-    print('hi')
-    # 탈출 조건 : 자릿수가 채워질 경우
-    if len(num_v) == d: print(num_v); return num_v
-    # 반복 분할
-    if (i <= row <= i + n//2) and (j + n//2 < col < j + n):
-        return make_num(n//2, i, j + n//2, num_v + '1')
-    elif (i <= row <= i + n//2) and (j <= col <= j + n//2):
-        return make_num(n//2, i, j, num_v + '2')
-    elif (i + n//2 < row < i + n) and (j <= col <= j + n//2):
-        return make_num(n//2, i + n//2, j, num_v + '3')
-    elif (i + n//2 < row < i + n) and (j + n//2 < col < j + n):
-        return make_num(n//2, i + n//2, j + n//2, num_v + '4')
+def make_num(n, r, c, ans=''):
+    if n < 1: print(ans)
+    elif r < n and c >= n: make_num(n//2, r, c-n, ans+'1')
+    elif r < n and c < n: make_num(n//2, r, c, ans+'2')
+    elif r >= n and c < n: make_num(n//2, r-n, c, ans+'3')
+    elif r >= n and c >= n: make_num(n//2, r-n, c-n, ans+'4')
 
-
-d, num = input().split(); d = int(d)
+d, num = input().split(); n = 2**(int(d)-1)
 x, y = map(int, input().split())
+row, col = get_idx(n)
 
-row, col = get_coor(2**d//2, 0, 0, 0)
-row += x; col += y
-print(make_num(2**d, 0, 0, ''))
+if 0 <= row < 2*n and 0 <= col < 2*n:
+    make_num(n, row, col)
+else:
+    print(-1)
 
-
+'''
+31120KB / 40ms
+'''
 
 
 # 메모리 초과
