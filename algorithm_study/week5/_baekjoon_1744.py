a8c0ab518ac2f4fef7578ba8cb0230fbeee0fe9c
lim kyoung tae <limkt0925@gmail.com>
2024-03-05 00:18:09 +0900

    240305 UPDATE

--- algorithm_study/week5/_baekjoon_1744.py
+++ algorithm_study/week5/_baekjoon_1744.py
@@ -2,3 +2,21 @@
 
 import sys 
 sys.stdin = open('input.txt') 
+
+N = int(input())
+max_v = 0
+num = sorted([int(input()) for _ in range(N)])
+
+if N != 1:
+    for i in range(0, N-1, 2):
+        if num[i] * num[i+1] == 1:
+
+        elif num[i] * num[i+1] > 0:
+            max_v += num[i] * num[i+1]
+        elif num[i] * num[i+1] < 0:
+            max_v += num[i] + num[i+1]
+        elif num[i] > 0 or num[i+1] > 0:
+            max_v += num[i] + num[i+1]
+    print(max_v)
+else:
+    print(*num)
--- algorithm_study/week5/baekjoon_14235.py
+++ algorithm_study/week5/baekjoon_14235.py
@@ -1,14 +1,16 @@
 # 14235 크리스마스 선물
-
-import sys
-sys.stdin = open('input.txt')
 import heapq
 
-gift = []
+gift = []  # 최대 힙 생성
 for _ in range(int(input())):
     n = list(map(int, input().split()))
     if n.pop(0):
-        heapq.heappush(gift, n)
+        for i in n:
+            heapq.heappush(gift, -i)
     else:
         if gift: print(-heapq.heappop(gift))
         else: print(-1)
+
+'''
+40920KB / 124ms
+'''
