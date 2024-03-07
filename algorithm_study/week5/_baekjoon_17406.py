5e1ee9ef7c5cd07f392dfda761f7cbc6ce27fada
lim_kyoung_tae <limkt0925@naver.com>
2024-03-07 17:59:09 +0900

    240307 UPDATE

--- algorithm_study/week5/_baekjoon_17406.py
+++ algorithm_study/week5/_baekjoon_17406.py
@@ -2,3 +2,10 @@
 
 import sys 
 sys.stdin = open('input.txt') 
+
+N, M, K = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+
+for _ in range(K):
+    r, c, s = map(int, input().split())
+print(A)
