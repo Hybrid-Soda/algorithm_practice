60f1321c524d7df16cdaf9334de202c4a1a49c14
limkt0925 <limkt0925@gmail.com>
2024-04-05 17:59:58 +0900

    240405 UPDATE

--- Study/week10/baekjoon_15681.py
+++ Study/week10/baekjoon_15681.py
@@ -2,3 +2,27 @@
 
 import sys
 sys.stdin = open('input.txt')
+
+def DFS(now):
+    visit[now] = 1
+    for next in tree[now]:
+        if not visit[next]:
+            DFS(next)
+            subtree[now] += subtree[next]
+    
+    subtree[now] = 1
+    return
+
+N, R, Q = map(int, input().split())
+tree = [[] for _ in range(N+1)]
+visit = [0] * (N+1)
+subtree = [0] * (N+1)
+
+for _ in range(N-1):
+    U, V = map(int, input().split())
+    tree[U].append(V)
+    tree[V].append(U)
+
+DFS(R)
+
+[print(subtree[int(input())]) for _ in range(Q)]
