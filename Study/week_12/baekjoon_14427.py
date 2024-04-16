adc5e75b340b816476ff58724d703f0b489353f8
lim kyoung tae <limkt0925@gmail.com>
2024-04-17 01:44:48 +0900

    240417 UPDATE

--- Study/week_12/baekjoon_14427.py
+++ Study/week_12/baekjoon_14427.py
@@ -1,29 +1,50 @@
 # 14427 수열과 쿼리 15
 
-class LinkArr:
-    def __init__(self, n, i):
-        self.n = n
-        self.i = i
-    
-    def __str__(self):
-        return f'{self.n}'
-
 import sys
-from heapq import heappop, heappush
 sys.stdin = open('input.txt')
 
+n = int(input())
+arr = list(map(int, input().split()))
 
-N = int(input())  # 1 <= N <= 100,000
-A = list(map(int, input().split()))  # 1 <= A <= 10^9
-tree = []
+m = int(input())
+qry = [list(map(int, input().split())) for _ in range(m)]
 
-for i in range(N):
-    heappush(tree, LinkArr(A[i], i).n)
+tree = [None] * (4*n)
 
-for _ in range(int(input())):  # 1 <= M <= 100,000
-    query = list(map(int, input().split()))
-    print(tree)
-    if query[0] == 1:
-        A[query[1]-1] = query[2]
+def init(node, start, end):
+    if start == end:
+        tree[node] = (arr[start], start+1)
     else:
-        print(tree[0][1])
+        mid = (start + end) // 2
+        init(node*2, start, mid)
+        init(node*2+1, mid+1, end)
+        tree[node] = min(tree[node*2], tree[node*2+1], key=lambda x: (x[0], x[1]))
+
+init(1, 0, n-1)
+
+def update(node, start, end, idx, value):
+    if start == end:
+        tree[node] = (value, idx + 1)
+    else:
+        mid = (start + end) // 2
+        if start <= idx <= mid:
+            update(node*2, start, mid, idx, value)
+        else:
+            update(node*2+1, mid+1, end, idx, value)
+        tree[node] = min(tree[node*2], tree[node*2+1], key=lambda x: (x[0], x[1]))
+
+result = []
+query_index = 0
+
+for _ in range(m):
+    query_type = int(qry[query_index])
+    if query_type == 1:
+        idx = int(qry[query_index + 1]) - 1
+        value = int(qry[query_index + 2])
+        update(1, 0, n-1, idx, value)
+        query_index += 3
+    elif query_type == 2:
+        result.append(str(tree[1][1]))
+        query_index += 1
+
+    print("\n".join(result))
--- Study/week_12/baekjoon_1799.py
+++ Study/week_12/baekjoon_1799.py
@@ -2,3 +2,36 @@
 
 import sys
 sys.stdin = open('input.txt')
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+visit = [[1-board[i][j] for j in range(N)] for i in range(N)]
+
+go = ((1, 1), (1, -1), (-1, 1), (-1, -1))
+ans = 0
+
+def check(i, j, put):
+    visit[i][j] += 2*put - 1
+    for k in range(1, N):
+        for di, dj in go:
+            ni = i + k*di
+            nj = j + k*dj
+            if 0 <= ni < N and 0 <= nj < N:
+                visit[ni][nj] += 2*put - 1
+
+def DFS(ni, nj, cnt):
+    global ans
+    if cnt + (N - ni) <= ans:
+        return
+
+    for i in range(ni, N):
+        for j in range(nj, N):
+            if not visit[i][j]:
+                check(i, j, 1)
+                DFS(i, j, cnt+1)
+                check(i, j, 0)
+        nj = 0
+    ans = max(ans, cnt)
+
+DFS(0, 0, 0)
+print(ans)
