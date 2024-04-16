8829203ba22c9b0e8a6d4124108c4e9a33c0971c
lim kyoung tae <limkt0925@gmail.com>
2024-04-16 17:59:08 +0900

    240416 UPDATE

--- Study/week_12/baekjoon_14427.py
+++ Study/week_12/baekjoon_14427.py
@@ -1,4 +1,29 @@
 # 14427 수열과 쿼리 15
 
+class LinkArr:
+    def __init__(self, n, i):
+        self.n = n
+        self.i = i
+    
+    def __str__(self):
+        return f'{self.n}'
+
 import sys
+from heapq import heappop, heappush
 sys.stdin = open('input.txt')
+
+
+N = int(input())  # 1 <= N <= 100,000
+A = list(map(int, input().split()))  # 1 <= A <= 10^9
+tree = []
+
+for i in range(N):
+    heappush(tree, LinkArr(A[i], i).n)
+
+for _ in range(int(input())):  # 1 <= M <= 100,000
+    query = list(map(int, input().split()))
+    print(tree)
+    if query[0] == 1:
+        A[query[1]-1] = query[2]
+    else:
+        print(tree[0][1])
