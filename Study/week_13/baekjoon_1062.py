b6cbc03ef890e613d6f14d37b1912ab8c3651b09
lim kyoung tae <limkt0925@gmail.com>
2024-04-21 23:42:12 +0900

    240421 UPDATE

--- Study/week_13/baekjoon_1062.py
+++ Study/week_13/baekjoon_1062.py
@@ -2,3 +2,17 @@
 
 import sys
 sys.stdin = open('input.txt')
+
+def change(word):
+    word = set(word)
+    for char in ['a', 'n', 't', 'i', 'c']:
+        word.remove(char)
+    return ''.join(word)
+
+N, K = map(int, input().split())
+words = [change(input()) for _ in range(N)]
+
+if K < 5: exit(print(0))
+
+
+print(words)
