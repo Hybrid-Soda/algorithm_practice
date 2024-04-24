7d806761fa9a56c415560f3dbe29e6c84a445aba
lim kyoung tae <limkt0925@gmail.com>
2024-04-25 00:03:49 +0900

    240425 UPDATE

--- Study/week_13/baekjoon_2357.py
+++ Study/week_13/baekjoon_2357.py
@@ -2,3 +2,41 @@
 
 import sys
 sys.stdin = open('input.txt')
+
+INF = float('inf')
+N, M = map(int, input().split())
+max_tree = [0 for _ in range(2*N)]
+min_tree = [INF for _ in range(2*N)]
+
+# 트리 삽입
+for i in range(N):
+    num = int(input())
+    max_tree[N+i] = num
+    min_tree[N+i] = num
+
+# 트리 정렬
+for i in range(N-1, 0, -1):
+    max_tree[i] = max(max_tree[i<<1], max_tree[i<<1|1])
+    min_tree[i] = min(min_tree[i<<1], min_tree[i<<1|1])
+
+# 쿼리 수행
+for _ in range(M):
+    a, b = map(int, input().split())
+    a += N-1
+    b += N-1
+    maxV = 0
+    minV = INF
+
+    while a < b:
+        if a & 1:
+            pass
+        if ~(b & 1):
+            pass
+        a >>= 1
+        b >>= 1
+    
+    if a == b:
+        maxV = max(maxV, max_tree[a])
+        minV = min(minV, min_tree[a])
+    
+    print(minV, maxV)
