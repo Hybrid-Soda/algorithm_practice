e9c36b96578137118119658613c6225830344d03
lim kyoung tae <limkt0925@gmail.com>
2024-04-25 17:58:36 +0900

    240425 UPDATE

--- Study/week_13/baekjoon_16946.py
+++ Study/week_13/baekjoon_16946.py
@@ -1,4 +1,30 @@
 # 16946 벽 부수고 이동하기 4
 
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
+
+go = ((0, 1), (1, 0), (0, -1), (-1, 0))
+N, M = map(int, input().split())
+grid = [list(map(int, input())) for _ in range(N)]
+
+def empty(i, j):
+    visit = [[0] * M for _ in range(N)]
+    Q = deque([[i, j]])
+
+    while Q:
+        i, j = Q.popleft()
+        for di, dj in go:
+            ni, nj = i + di, j + dj
+            if 0<=ni<N and 0<=nj<M and not grid[ni][nj] and not visit[ni][nj]:
+                visit[ni][nj] = 1
+                Q.append([ni, nj])
+
+for i in range(N):
+    for j in range(M):
+        if grid[i][j]:
+            print(wall(), end='')
+        else:
+            empty(i, j)
+            print(0, end='')
+    print()
