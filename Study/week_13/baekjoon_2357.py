29cb94d9c7768130118cd777a780d9ef51a0fbbe
lim kyoung tae <limkt0925@gmail.com>
2024-04-30 01:31:34 +0900

    240430 UPDATE

--- Study/week_13/baekjoon_2357.py
+++ Study/week_13/baekjoon_2357.py
@@ -1,25 +1,25 @@
 # 2357 최솟값과 최댓값
 
-import sys
-sys.stdin = open('input.txt')
-
 INF = float('inf')
 N, M = map(int, input().split())
-max_tree = [0 for _ in range(2*N)]
-min_tree = [INF for _ in range(2*N)]
 
-# 트리 삽입
+# 노드 개수 : 2N-1
+maxTree = [0 for _ in range(2*N)]
+minTree = [INF for _ in range(2*N)]
+
+# 트리 삽입 : 우선 리프 노드에만 숫자들 삽입 (리프 노드 순서가 인덱스)
 for i in range(N):
     num = int(input())
-    max_tree[N+i] = num
-    min_tree[N+i] = num
+    maxTree[N+i] = num
+    minTree[N+i] = num
 
-# 트리 정렬
+# 트리 정렬 : 두 자식 노드의 숫자 크기를 비교하여 부모 노드에 올려보냄
 for i in range(N-1, 0, -1):
-    max_tree[i] = max(max_tree[i<<1], max_tree[i<<1|1])
-    min_tree[i] = min(min_tree[i<<1], min_tree[i<<1|1])
+    # i<<1 = 2*i / i<<1|1 = 2*i+1
+    maxTree[i] = max(maxTree[i<<1], maxTree[i<<1|1])
+    minTree[i] = min(minTree[i<<1], minTree[i<<1|1])
 
-# 쿼리 수행
+# 쿼리 수행 : a번째부터 b번째까지 중 최솟값, 최댓값 정수 구하기
 for _ in range(M):
     a, b = map(int, input().split())
     a += N-1
@@ -28,19 +28,22 @@ for _ in range(M):
     minV = INF
 
     while a < b:
+        # a번째 노드가 왼쪽 자식 노드인 경우 (a 인덱스: 홀수)
         if a & 1:
-            max_res = max(max_res, max_tree[a])
-            min_res = min(min_res, min_tree[a])
-            a += 1
+            maxV = max(maxV, maxTree[a])
+            minV = min(minV, minTree[a])
+            a += 1  # 다른 가지를 탐색하기 위해 (+)
+        # b번째 노드가 오른쪽 자식 노드인 경우 (b 인덱스: 짝수)
         if ~(b & 1):
-            max_res = max(max_res, max_tree[b])
-            min_res = min(min_res, min_tree[b])
-            b -= 1
+            maxV = max(maxV, maxTree[b])
+            minV = min(minV, minTree[b])
+            b -= 1  # 다른 가지를 탐색하기 위해 (-)
+        # 부모 노드로 이동
         a >>= 1
         b >>= 1
-    
+
     if a == b:
-        maxV = max(maxV, max_tree[a])
-        minV = min(minV, min_tree[a])
-    
+        maxV = maxTree[a]
+        minV = minTree[a]
+
     print(minV, maxV)
